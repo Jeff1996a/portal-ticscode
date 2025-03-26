dec69d284e92627b74e50724e53de5d30319cadf
Jeff1996a <alfaro.jefferson96@gmail.com>
2025-03-26 13:22:14 -0500

    emojis

--- app/components/forms/register-form.tsx
+++ app/components/forms/register-form.tsx
@@ -14,7 +14,8 @@ const countryOptions = [
   { value: "+91", label: "🇮🇳 +91 (India)" },
   { value: "+61", label: "🇦🇺 +61 (Australia)" },
   { value: "+81", label: "🇯🇵 +81 (Japan)" },
-  { value: "+593", label: "EC +593 (Ecuador)" },
+  { value: "+593", label: "🇪🇨 +593 (Ecuador)" },
+  { value: "+501", label: "🇧🇿 +593 (Ecuador)" },
 ];
 
 export default function RegisterForm(){
